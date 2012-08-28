3f550a7d50314e8e8a1bb95d860e5816421aa05d
Jonathan Linowes <linojon@gmail.com>
2012-08-28 14:49:52 -0400

    list full_name (not last, first), allow dot in name (for initials)

--- lib/extras/proper_name_validator.rb
+++ lib/extras/proper_name_validator.rb
@@ -1,6 +1,6 @@
 class ProperNameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A[a-zA-Z\'\-\x20]+\z/
+    unless value =~ /\A[a-zA-Z\'\-\.\x20]+\z/
       record.errors[attribute] << (options[:message] || "is not a proper name format")
     end
   end
